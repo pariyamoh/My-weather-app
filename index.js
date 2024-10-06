8c55d4057f62c2acccd4c0790736a54b99db7682
pariya <pariyamohammadi1373@gmail.com>
2024-10-06 14:06:24 +0200

    adding more id

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-alert =
-    hello;
+function handleSearchSubmit(event){
+    event.preventDefualt();
+    let searchInput = document.querySelector("#showing");
+}
+
+let searchFormElement = document.querySelector("#searching");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
