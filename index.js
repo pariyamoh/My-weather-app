3142fb13cb8b795f4723d2bb044855d2e97e6196
pariya <pariyamohammadi1373@gmail.com>
2024-10-07 21:23:06 +0200

    adding the temperature more details

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
 function bringTemp (response){
 let temperatureElement = document.querySelector("#degree");
-let degree = response.data
+let degree = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+temperatureElement.innerHTML = Math.round(degree);
 }
 
 
-
-function searchPlace (place){
+function searchPlace (city){
 let apiKey = "94a4oafdtf0d380ca243ac81b53c4ce3";
-  
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${place}&key=${apiKey}
-console.log(apiUrl)`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`
 axios.get(apiUrl).then(bringTemp);
 }
+
+
 function showingName (event){
   event.preventDefault();
   let searchBox = document.querySelector("#search-box");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchBox.value;
   searchPlace(searchBox.value);
 }
 
+
+
 let searchFormElement = document.querySelector("#searching");
 searchFormElement.addEventListener("submit", showingName);
+
+
+searchPlace("Tehran");
