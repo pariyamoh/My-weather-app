a5d1b1ed50e021784aa14c99ae213007d1af6d57
pariya <pariyamohammadi1373@gmail.com>
2024-10-08 14:51:08 +0200

    adding more like days and hours

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ let cityElement = document.querySelector("#city");
 let conditionElement = document.querySelector("#condition");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector ("#wind");
+let timesElement = document.querySelector ("#times");
+let date = new Date(response.data.time * 1000);
+
+
 
 
 cityElement.innerHTML = response.data.city;
@@ -12,8 +16,30 @@ temperatureElement.innerHTML = Math.round(degree);
 conditionElement.innerHTML = response.data.condition.description;
 humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 windElement.innerHTML  = `${response.data.wind.speed}Km/h`;
+timesElement.innerHTML = formatDate(date);
 
+}
 
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+    let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 
